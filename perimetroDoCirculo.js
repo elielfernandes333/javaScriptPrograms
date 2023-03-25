0f1187a79bacdafd42d3a16b62cb2063aa7d775e
3lielFernandes <123779747+elielfernandes333@users.noreply.github.com>
2023-03-25 11:34:24 -0300

    Create perimetroDoCirculo.js

---
+++ perimetroDoCirculo.js
@@ -0,0 +1,7 @@
+//Perimetro do círculo de raio R
+
+var r = prompt("Digite o Valor do raio R");
+var PI = 3.14159265;
+var perimetro = 2*PI*r;
+
+alert("O perímetro do círculo de raio "+r+" é "+perimetro.toFixed(2))
